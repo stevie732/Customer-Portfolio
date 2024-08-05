0792689c74cb53a636a502c5f40da0fdf7dec66b
Stephen <stephenprahljr@outlook.com>
2024-08-05 16:11:28 -0400

    'updated & unfinished'

--- main.js
+++ main.js
@@ -5,3 +5,26 @@ window.addEventListener('scroll', function() {
     document.querySelector('.navbar').classList.remove('scrolled');
   }
 });
+
+// makes the image Pop Out
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = 'block';
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = 'none';
+}
+
+// Carousel popout
+doument.addEventListener('DOMContentLoaded');
+
+window.addEventListener('load', () => {
+  const carousel = document.getElementById('projectCarousel');
+  carousel.style.opacity = '0'; // Initially hides the carousel
+
+  setTimeout(() => {
+    carousel.style.transition = 'opacity 0.5s ease, transform 1s ease'; // Adds a transition for opacity
+    carousel.style.opacity = '1'; // Fades in the carousel
+    carousel.style.transform = 'translateZ(20px)'; // Apply's the initial pop-out transform
+  }, 500); // Adjusts the delay (in milliseconds) as needed
+});
